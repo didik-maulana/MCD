f518a9a2dd369df9d814b76b7466685b62f73270
Didik Maulana Ardiansyah <didikmaulana49@gmail.com>
2019-06-26 10:33:22 +0700

    initialize start project

---
+++ app/src/main/java/com/didik/mcd/MainActivity.java
@@ -0,0 +1,13 @@
+package com.didik.mcd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
